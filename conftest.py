2b5f32b06ede3607dd9b76b94a34455ccdecea95
Gabriela <gabrielacunarro@gmail.com>
2026-05-06 12:56:38 -0400

    agregue webdriverwait en el carrito

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 
 @pytest.fixture()
-def test_driver():
+def driver():
     driver = webdriver.Chrome()
     driver.get("http://www.saucedemo.com/")
     yield driver
--- tests/test_carrito.py
+++ tests/test_carrito.py
@@ -1,5 +1,7 @@
 from src.helpers import login
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_carrito(driver):
     login(driver)
@@ -12,11 +14,16 @@ def test_carrito(driver):
     nombre_producto = primer_producto.find_element(By.CLASS_NAME, "inventory_item_name").text
 
     # Hacer click en add to cart del primer producto.
-    boton_add = primer_producto.find_element(By.CLASS_NAME, "btn_inventory")
+    boton_add = primer_producto.find_element(By.TAG_NAME, "button")
     boton_add.click()
 
-    # Verificar que el contador del carrito sea 1.
+    # Esperar a que aparezca el contador del carrito.
     carrito = driver.find_element(By.ID, "shopping_cart_container")
+
+    WebDriverWait(driver, 5).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "shopping_cart_badge"))
+    )
+
     contador = carrito.find_element(By.CLASS_NAME, "shopping_cart_badge").text
     assert contador == "1", "El contador del carrito no se incrementó correctamente."
 
