edb256b2d05ace9d72ce694d65a55658d0de3089
Gabriela <gabrielacunarro@gmail.com>
2026-06-22 08:45:22 -0400

    Remove old tests and fix merge conflicts

--- utils/data_reader.py
+++ utils/data_reader.py
@@ -2,15 +2,10 @@ import csv
 import json
 
 def read_users_csv():
-    with open("data/users.csv",newline="") as file:
+    with open("data/users.csv", newline="") as file:
         reader = csv.DictReader(file)
         return list(reader)
     
 def read_products_json():
     with open("data/products.json") as file:
-<<<<<<< HEAD
         return json.load(file)
-=======
-        return json.load(file)
-    
->>>>>>> e3d064a7da372905a51d3fe2351190578c1b957c
